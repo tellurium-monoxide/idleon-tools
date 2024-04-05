119efed4e4a018796e5231d4be6eda127ad41521
Téo - Buukz <teo.boutin@proton.me>
2024-04-06 00:04:40 +0200

    add python server script for testing

---
+++ Server.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+#test on python 3.4 ,python of lower version  has different module organization.
+import http.server
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import socketserver
+
+PORT = 8000
+
+
+def run():
+    Handler = http.server.SimpleHTTPRequestHandler
+    
+    Handler.extensions_map={
+            '.manifest': 'text/cache-manifest',
+            '.html': 'text/html',
+            '.png': 'image/png',
+      '.jpg': 'image/jpg',
+      '.svg': 'image/svg+xml',
+      '.css': 'text/css',
+      # '.js':  'application/x-javascript',
+      '.js':  'text/javascript',
+      '': 'application/octet-stream', # Default
+        }
+    
+    with socketserver.TCPServer(("", PORT), Handler) as httpd:
+        print("serving at port", PORT)
+        httpd.serve_forever()
+
+
+
+
+if __name__ == '__main__':
+    run()
