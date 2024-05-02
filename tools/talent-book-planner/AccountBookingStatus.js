105901a9552d61b8a871a83c48d19dfc24b051be
tellurium-monoxide <teo.boutin@proton.me>
2024-05-02 18:13:37 +0200

    improve talent and tier dispays

--- tools/talent-book-planner/AccountBookingStatus.js
+++ tools/talent-book-planner/AccountBookingStatus.js
@@ -45,7 +45,7 @@ class AccountBookingStatus {
         }
 
         this.makeTalentUpgradeList()
-        this.displayTiers()
+
 
     }
 
@@ -106,7 +106,7 @@ class AccountBookingStatus {
     getTalentDisplay(player, talent) {
         let display = ""
         let icon = TALENT_ICONS[talent.name]
-        let is_tiered = isTiered(talent.name) ? "" : ">>"
+        let is_tiered = isTiered(talent.name, player.class_name) ? "" : ">>"
         let icon_display = icon ? `<img src=${icon}/>` : (talent.name)
         // let icon_display = icon ? `<img src=${icon}/>` : capEachWord(talent.name)
         let max_lvl = player.skill_max_levels[talent.skillIndex]
@@ -259,61 +259,6 @@ class AccountBookingStatus {
 
 
 
-    displayTiers() {
-        let max_tier = TALENT_TIERS.length
-
-        let tabs = $("#tabs-all-tiers").tabs();
-        tabs.find("div").remove();
-        tabs.find("li").remove();
-        for (let tier = 0; tier < max_tier; tier++) {
-
-
-            // add tier tab
-            let tier_name = `Tier ${tier + 1}`
-            let li = `<li><a href='#tab_tier_display${tier}'>${tier_name}</a> </li>`
-
-            let content = ""
-            if (tier < max_tier) {
-                content += `${TALENT_TIERS[tier].purpose}`
-
-            } else {
-                content += `Bad or not yet added to a tier or not needed because maxed on another char`
-            }
-            content += `<table class="tiered_talents">`
-            content += `<tr>`
-            content += `<th>Talent</th>`
-            content += `<th>Purpose</th>`
-            content += `<th>Is global?</th>`
-            content += `<th>Class</th>`
-            content += `</tr>`
-
-            for (let tiered_talent of Object.entries(TALENT_TIERS[tier].list)) {
-
-                let global = tiered_talent[1].global ? "yes" : "no"
-                let class_restrict = tiered_talent[1].class_restrict || ""
-                content += `<tr>`
-                content += `<td>${FormatWords(tiered_talent[0])}</td>`
-                content += `<td>${tiered_talent[1].purpose}</td>`
-                content += `<td>${global}</td>`
-                content += `<td>${FormatWords(class_restrict)}</td>`
-                content += `</tr>`
-            }
-
-            content += `</table>`
-
-
-
-            tabs.find("#tabs-all-tiers-nav").append(li);
-            tabs.append(`<div id="tab_tier_display${tier}">` + content + "</div>");
-            tabs.tabs("refresh");
-            tabs.tabs("option", "heightStyle", "auto");
-            tabs.tabs("option", "active", 0);
-
-
-        }
-
-
-    }
 }
 
 
