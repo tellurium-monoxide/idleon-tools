5f14ad0ca858d80dc56b61a5ecbbf1df468d1b40
tellurium-monoxide <teo.boutin@proton.me>
2024-08-18 14:55:19 +0200

    started porting to using complexity framework

---
+++ build_and_serve.py
@@ -0,0 +1,20 @@
+from complexity.prep import prompt_and_delete_cruft
+from complexity.main import complexity
+from complexity.serve import serve_static_site
+
+import os
+import sys
+import shutil
+
+mydir = 'www/'
+
+# Try to remove the tree; if it fails, throw an error using try...except.
+try:
+    shutil.rmtree(mydir)
+except OSError as e:
+    print("Error: %s - %s." % (e.filename, e.strerror))
+    
+
+complexity('project/', 'www/')
+serve_static_site('www/')
+
