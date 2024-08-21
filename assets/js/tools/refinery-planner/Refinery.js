fe11db708870b6ec3184b758c3a47717f88a2a8d
tellurium-monoxide <teo.boutin@proton.me>
2024-08-21 17:56:34 +0200

    add resource generation display

--- assets/js/tools/refinery-planner/Refinery.js
+++ assets/js/tools/refinery-planner/Refinery.js
@@ -9,6 +9,8 @@ class Refinery {
         console.log(refinery_data)
 
         this.salts = Array(6).fill().map((_, i) => ({}));
+        this.resources = []
+        this.resource_generation = {} // per hour
         for (let salt_index = 0; salt_index < 6; salt_index++) {
             let salt = this.salts[salt_index]
             const rank = refinery_data[salt_index + 3][1]
@@ -20,13 +22,23 @@ class Refinery {
             salt.cyclesPerRank = Math.ceil(salt.powerToRankUp / salt.powerPerCycle)
             salt.NextCycleBreakpoint = this.calcNextCycleBreakpoint(rank)
             for (let material of salt.data.material_costs) {
+                this.resources.push(JSON.parse(JSON.stringify(material)))
                 const isSalt = material.name.includes("Salts")
                 material.costPerCycle = material.baseValue * Math.floor(Math.pow(rank, (isSalt) ? 1.3 : 1.5))
                 material.costPerRank = material.costPerCycle * salt.cyclesPerRank
+
+            }
+            this.resource_generation[salt.data.name] = salt.powerPerCycle
+        }
+
+        for (let material of this.resources) {
+            if (!this.resource_generation[material.name]) {
+                this.resource_generation[material.name] = 0
             }
         }
 
         this.showRefinery()
+        this.showResources()
 
     }
 
@@ -60,18 +72,21 @@ class Refinery {
         content += `<th></th>`
         content += `<th>Rank</th>`
         content += `<th>Progress</th>`
-        content += `<th>Power per cycle<br>(Power per hour)</th>`
+        content += `<th>Power per cycle<br>(next rank|increase)</th>`
         content += `<th>Cycles per rank<br>(next breakpoint)</th>`
         content += `<th>Cost per rank</th>`
         content += "</tr>"
 
         for (const salt of this.salts) {
 
+
             content += "<tr>"
             content += `<td><img src="${salt.data.icon_url}"></td>`
             content += `<td>${salt.rank}</td>`
             content += `<td class="progress">${salt.progress.toLocaleString().padStart(9, " ")}/${salt.powerToRankUp.toLocaleString().padEnd(9, " ")}<br>(${formatPercent(salt.progress / salt.powerToRankUp)})</td>`
-            content += `<td>${salt.powerPerCycle}<br>(?)</td>`
+
+            let nextRankPowerPerCycle = this.getPowerPerCycle(salt.rank + 1)
+            content += `<td>${salt.powerPerCycle}<br>(${nextRankPowerPerCycle}|+${nextRankPowerPerCycle - salt.powerPerCycle})</td>`
             content += `<td>${salt.cyclesPerRank}<br>(${salt.NextCycleBreakpoint.cyclesPerRank} at r${salt.NextCycleBreakpoint.rank})</td>`
             content += `<td><table class="costs"><tr>`
             let i = 0
@@ -90,12 +105,28 @@ class Refinery {
         document.getElementById("refinery").innerHTML = content;
 
     }
+
+    showResources() {
+        let content = ""
+        for (let material of this.resources) {
+            content += "<div><table>"
+            content += "<tr>"
+            content += `<td><img src="${material.icon}"></td>`
+            content += `<td>${this.resource_generation[material.name]}/h</td>`
+            content += "</tr>"
+            content += "</table>"
+            content += "</div>"
+        }
+
+
+        document.getElementById("resources").innerHTML = content;
+    }
 }
 const formatPercent = (percent) => `${(percent * 100).toFixed(2)}%`.padStart(6, "0");
 
 const SALT_DATA = [
     {
-        name: "Redox",
+        name: "Redox_Salts",
         icon_url: "https://idleon.wiki/wiki/images/thumb/c/cb/Redox_Salts.png/36px-Redox_Salts.png",
         material_costs: [
             { name: "Spore_Cap", icon: "https://idleon.wiki/wiki/images/thumb/7/7c/Spore_Cap.png/50px-Spore_Cap.png", baseValue: 10 },
@@ -103,7 +134,7 @@ const SALT_DATA = [
         ]
     },
     {
-        name: "Explosive",
+        name: "Explosive_Salts",
         icon_url: "https://idleon.wiki/wiki/images/thumb/2/2f/Explosive_Salts.png/36px-Explosive_Salts.png",
         material_costs: [
             { name: "Bullforg_Horn", icon: "https://idleon.wiki/wiki/images/thumb/5/56/Bullfrog_Horn.png/50px-Bullfrog_Horn.png", baseValue: 10 },
@@ -112,7 +143,7 @@ const SALT_DATA = [
         ]
     },
     {
-        name: "Spontaneity",
+        name: "Spontaneity_Salts",
         icon_url: "https://idleon.wiki/wiki/images/thumb/3/3e/Spontaneity_Salts.png/36px-Spontaneity_Salts.png",
         material_costs: [
             { name: "Pocket_Sand", icon: "https://idleon.wiki/wiki/images/thumb/8/8c/Pocket_Sand.png/50px-Pocket_Sand.png", baseValue: 50 },
@@ -122,7 +153,7 @@ const SALT_DATA = [
         ]
     },
     {
-        name: "Dioxide",
+        name: "Dioxide_Salts",
         icon_url: "https://idleon.wiki/wiki/images/thumb/9/96/Dioxide_Synthesis.png/36px-Dioxide_Synthesis.png",
         material_costs: [
             { name: "Floof_Ploof", icon: "https://idleon.wiki/wiki/images/thumb/0/08/Floof_Ploof.png/50px-Floof_Ploof.png", baseValue: 10 },
@@ -132,7 +163,7 @@ const SALT_DATA = [
         ]
     },
     {
-        name: "Purple",
+        name: "Purple_Salts",
         icon_url: "https://idleon.wiki/wiki/images/thumb/f/fb/Purple_Salt.png/36px-Purple_Salt.png",
         material_costs: [
             { name: "Cracked_Glass", icon: "https://idleon.wiki/wiki/images/thumb/2/20/Cracked_Glass.png/50px-Cracked_Glass.png", baseValue: 25 },
@@ -143,7 +174,7 @@ const SALT_DATA = [
         ]
     },
     {
-        name: "Nullo",
+        name: "Nullo_Salts",
         icon_url: "https://idleon.wiki/wiki/images/thumb/a/a1/Nullo_Salt.png/36px-Nullo_Salt.png",
         material_costs: [
             { name: "Contact_Lense", icon: "https://idleon.wiki/wiki/images/thumb/4/48/Contact_Lense.png/50px-Contact_Lense.png", baseValue: 50 },
