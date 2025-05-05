ce3da2ae124d4e5c55ebed24a4e3d2ca8b3a6a73
tellurium-monoxide <teo.boutin@proton.me>
2025-05-05 11:01:37 +0200

    update atoms

--- assets/js/game/world3/Atoms.js
+++ assets/js/game/world3/Atoms.js
@@ -39,7 +39,35 @@ export class Atoms extends BaseFeature {
         return this.atom_levels.reduce((a, b) => (a + b), 0)
     }
     getDisplay() {
-        // TODO
+        let display = document.createElement("table")
+        for (const [ind, atom] of DATA_ATOMS.entries()) {
+
+            let level = this.atom_levels[ind]
+            let upg = DATA_ATOMS[ind]
+            let [name, stat_per_lvl, cost_data] = upg
+            let row = display.appendChild(document.createElement("tr"))
+
+            let name_cell = row.appendChild(document.createElement("td"))
+            name_cell.innerText = `${name.substring(0, name.indexOf("_"))}`
+
+
+            let input_cell = row.appendChild(document.createElement("td"))
+
+            let input_base = input_cell.appendChild(document.createElement("input"))
+            input_base.type = "number"
+            input_base.min = 0
+            input_base.max = 50
+            input_base.value = level
+
+            new InputSpinner(input_base)
+            input_base.addEventListener("input", (event) => {
+                console.log("change atom level", name, this.atom_levels[ind], "to", Number(input_base.value))
+                this.atom_levels[ind] = Number(input_base.value)
+                this.account.setModifiedFromSaveData()
+            });
+        }
+
+        return display
     }
 }
 
@@ -51,8 +79,10 @@ export const DATA_ATOMS = [
     ["BORON_-_PARTICLE_UPGRADER", 2, [70, 1.37, 175]],
     ["CARBON_-_WIZARD_MAXIMIZER", 2, [250, 1.27, 500]],
     ["NITROGEN_-_CONSTRUCTION_TRIMMER", 0.15, [500, 1.25, 1000]],
-    ["OXYGEN_-_LIBRARY_BOOKER", 0.02, [2000, 1.24, 3250]],
-    ["FLUORIDE_-_VOID_PLATE_CHEF", 0.01, [12000, 1.23, 10000]],
-    ["NEON_-_DAMAGE_N'_CHEAPENER", 0.01, [40000, 1.22, 40000]],
-    ["SODIUM_-_SNAIL_KRYPTONITE", 5, [50000, 2, 50000]]
+    ["OXYGEN_-_LIBRARY_BOOKER", 0.02, [1000, 1.24, 3250]],
+    ["FLUORIDE_-_VOID_PLATE_CHEF", 0.01, [2500, 1.23, 2500]],
+    ["NEON_-_DAMAGE_N'_CHEAPENER", 0.01, [5000, 1.22, 5000]],
+    ["SODIUM_-_SNAIL_KRYPTONITE", 5, [12000, 2, 12000]],
+    ["MAGNESIUM_-_TRAP_COMPOUNDER", 0.01, [30000, 1.6, 30000]],
+    ["ALUMINIUM_-_STAMP_SUPERCHARGER", 2, [200000, 1.3, 200000]],
 ];
